3bd7cd1432471e78e99475f59aa347494a7e0238
Parth <maitraparth@gmail.com>
2024-04-28 21:23:01 +0530

    Sum of BST in Range

---
+++ src/main/java/SumOfBSTInRange.java
@@ -0,0 +1,15 @@
+public class SumOfBSTInRange {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        int sum=0;
+        if(root.val>=low&&root.val<=high){
+            sum+=root.val;
+        }
+        if(root.right!=null){
+            sum+=rangeSumBST(root.right,low,high);
+        }
+        if(root.left!=null){
+            sum+=rangeSumBST(root.left,low,high);
+        }
+        return sum;
+    }
+}
