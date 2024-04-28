49d91ffaeef2e55f5aa7a286b02aff6fced6d131
Parth <maitraparth@gmail.com>
2024-04-28 19:02:01 +0530

    Find a pair of numbers that equal to the other number

---
+++ src/main/java/TwoSum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class TwoSum {
+    public List<int[]> pairSum(int[] arr, int s) {
+        List<int[]> ans = new ArrayList<>();
+        HashMap<Integer,Integer> diff = new HashMap<>();
+        int curr;
+        for(int i=0;i<arr.length;i++){
+            curr=s-arr[i];
+            if(diff.get(arr[i])!=null){
+                int [] match = new int[]{arr[i],curr};
+                ans.add(match);
+            }
+            diff.put(curr,i);
+        }
+        return ans;
+    }
+}
