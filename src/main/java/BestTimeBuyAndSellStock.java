3fac9817cc521bb4ef64b60e510c7e1d33c3b6c8
Parth <maitraparth@gmail.com>
2024-08-16 23:40:12 +0530

    Best time to Buy and Sell Stock

---
+++ src/main/java/BestTimeBuyAndSellStock.java
@@ -0,0 +1,18 @@
+public class BestTimeBuyAndSellStock {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int buyAmt = Integer.MAX_VALUE;
+        for(int i = 0; i < prices.length; i++)
+        {
+            if(buyAmt > prices[i])
+            {
+                buyAmt = prices[i];
+            }
+            else if (profit < (prices[i] - buyAmt))
+            {
+                profit = prices[i] - buyAmt;
+            }
+        }
+        return profit;
+    }
+}
