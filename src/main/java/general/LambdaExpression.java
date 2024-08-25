faba4bc2dee15c7ba75e1313ae33c20f13b5c12d
parthmaitra <maitraparth@gmail.com>
2024-08-25 19:08:31 +0530

    Lambda Expression Practice

--- src/main/java/general/LambdaExpression.java
+++ src/main/java/general/LambdaExpression.java
@@ -2,38 +2,47 @@ package general;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LambdaExpression {
     public static void main(String[] args) {
-        SumOfTwoNumbers sum = (a,b)-> a+b;
+        SumOfTwoNumbers sum = (a, b) -> a*b;
         System.out.println(sum.sum(7,4));
         List<Integer> integerList = Arrays.asList(1,2,3,4,5,6);
-        System.out.println(integerList.stream().reduce((a,b)->a+b).get());
+        System.out.println(integerList.stream().reduce((a,b)->a*b).get());
+        System.out.println(integerList.stream().max((a,b)->b-a).get());
         System.out.println(integerList.stream().filter(a->a%2==0).collect(Collectors.toList()));
         System.out.println(integerList.stream().map(a->a*a).collect(Collectors.toList()));
         System.out.println(integerList.stream().sorted((a,b)->b-a).collect(Collectors.toList()));
-        integerList.stream().skip(4).forEach(System.out::println);
+        //integerList.stream().skip(2).forEach(System.out::println);
+        integerList.stream().collect(Collectors.toMap(e->e,e->e*e)).forEach((k,v)->System.out.print(k+":"+v+" "));
+        HashMap<Integer,String> hashMap = new HashMap<>();
+    }
+}
 
-    }
-}
-class Person {
-    String key,value;
-    public static void main(String[] args) {
-        List<Person> people = new ArrayList<>();
-        Person p = new Person();
-        p.key="problemstatement";
-        p.value="Check engine light is on";
-        people.add(p);
-        p = new Person();
-        p.key="Wororder";
-        p.value="MNT_DIDS_WO_912032931";
-        people.add(p);
-        people.stream().forEach(e->System.out.println(e.key+" "+e.value));
-    }
-}
 @FunctionalInterface
 interface SumOfTwoNumbers {
     int sum(int a,int b);
+    default void printSome(){
+        System.out.println("Print");
+    }
+}
+class SumOfTwoNumbersImpl implements SumOfTwoNumbers{
+
+    @Override
+    public int sum(int a, int b) {
+        return 0;
+    }
+
+    @Override
+    public void printSome() {
+        //SumOfTwoNumbers.super.printSome();
+        System.out.println("Print child");
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
