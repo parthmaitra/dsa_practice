68eed91ae4e0ad6721af23c2724ea543d3fa87e6
parthmaitra <maitraparth@gmail.com>
2024-08-26 21:56:52 +0530

    Lambda Expression changes

--- src/main/java/general/LambdaExpression.java
+++ src/main/java/general/LambdaExpression.java
@@ -1,6 +1,5 @@
 package general;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -8,16 +7,23 @@ import java.util.stream.Collectors;
 
 public class LambdaExpression {
     public static void main(String[] args) {
-        SumOfTwoNumbers sum = (a, b) -> a*b;
+        SumOfTwoNumbers sum = (a, b) -> a+b;
         System.out.println(sum.sum(7,4));
-        List<Integer> integerList = Arrays.asList(1,2,3,4,5,6);
+        List<Integer> integerList = Arrays.asList(1,2,3,4,5,6,5,6,9,1);
         System.out.println(integerList.stream().reduce((a,b)->a*b).get());
         System.out.println(integerList.stream().max((a,b)->b-a).get());
         System.out.println(integerList.stream().filter(a->a%2==0).collect(Collectors.toList()));
         System.out.println(integerList.stream().map(a->a*a).collect(Collectors.toList()));
         System.out.println(integerList.stream().sorted((a,b)->b-a).collect(Collectors.toList()));
-        //integerList.stream().skip(2).forEach(System.out::println);
-        integerList.stream().collect(Collectors.toMap(e->e,e->e*e)).forEach((k,v)->System.out.print(k+":"+v+" "));
+        integerList.stream().skip(2).forEach(e->System.out.print(e+" "));
+        integerList.stream().distinct().collect(Collectors.toMap(e->e,e->e*e)).forEach((k,v)->System.out.print(k+":"+v+" "));
+        System.out.println();
+        integerList.stream().limit(3).forEach(e-> System.out.print(e+" "));
+        System.out.println();
+        integerList.stream().max((a,b)->a-b).ifPresent(System.out::println);
+        integerList.stream().distinct().collect(Collectors.toList()).forEach(e-> System.out.print(e+" "));
+        System.out.println();
+        integerList.stream().peek(System.out::print).peek(e-> System.out.print(":")).forEach(e-> System.out.println(e*e));
         HashMap<Integer,String> hashMap = new HashMap<>();
     }
 }
